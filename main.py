2926a826af19ba4e981762c659a16d05b48bbb55
annona17 <luna207sn@gmail.com>
2023-10-18 09:43:30 +0700

    Continue

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password =='admin':
+            return redirect(url_for('home'))
+        else:
+            return 'Dang nhap that bai'
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
